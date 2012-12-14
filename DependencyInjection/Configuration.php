f07ed0f2fadc69adc996c82f6dd8a4b80ed12c46
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-12-14 14:13:12 +0200

    Fix bundle name.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('svt_main');
+        $rootNode = $treeBuilder->root('xi_sms');
 
         $rootNode
             ->children()
